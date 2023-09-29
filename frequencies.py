c2fb9d4b55ffd74f7d255d2286e555c79b640f81
Juntong Deng <juntong.deng@kcl.ac.uk>
2023-09-29 18:18:29 +0800

    Add frequency counter

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,8 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    items = [str(x) for x in items]
+    frequencies = dict.fromkeys(items)
+    for key in frequencies.keys():
+        frequencies[key] = items.count(key)
     return frequencies
